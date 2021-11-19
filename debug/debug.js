58e150e8f973e70dda861ebf6d94f1ebdf1c7209
Carter Medlin <carterm@users.noreply.github.com>
2021-11-19 12:19:34 -0800

    Initial working commit

--- debug/debug.js
+++ debug/debug.js
@@ -1,11 +1,75 @@
+//@ts-check
 //Loading environment variables
 const { Values } = require("../local.settings.json");
 Object.keys(Values).forEach(x => (process.env[x] = Values[x])); //Load local settings file for testing
 
+// @ts-ignore
 process.env.debug = true; //set to false or remove to run like the real instance
 
-//run the indexpage async
-const indexCode = require("./index");
+const treePush = require("@cagov/github-tree-push");
+
 (async () => {
-  return await indexCode();
+  /** @type {Map<string,*>} */
+  const fileMap = new Map();
+
+  const token = process.env["GITHUB_TOKEN"];
+  const tree1 = new treePush(token, {
+    owner: "cagov",
+    repo: "automation-development-target",
+    base: "github-tree-push-testing",
+    path: "github-tree-push",
+    deleteOtherFiles: true,
+    contentToBlobBytes: 1
+  });
+
+  const suffix = "1"; //new Date().toString();
+
+  fileMap.set("A/A/fileAA1.txt", "A" + suffix);
+  fileMap.set("A/A/fileAA2.txt", "B" + suffix);
+  fileMap.set("A/B/fileAB1.txt", "C" + suffix);
+  fileMap.set("A/B/fileAB2.txt", "D" + suffix);
+  tree1.addFile("Target File.txt", "E" + suffix);
+
+  tree1.addFile(
+    "Special Path옹엄얼언웅워원월/Special File옹엄얼언웅워원월.txt",
+    "some data 옹엄얼언웅워원월"
+  );
+
+  tree1.addFileMap(fileMap);
+  await tree1.treePush();
+
+  console.log(JSON.stringify(tree1.lastRunStats, null, 2));
+
+  const rootTree = new treePush(token, {
+    owner: "cagov",
+    repo: "automation-development-target",
+    base: "github-tree-push-testing",
+    recursive: false,
+    deleteOtherFiles: true,
+    commit_message: "Root Tree Push",
+    pull_request: true,
+    pull_request_options: {
+      draft: false,
+      body: "Pull Request Body",
+      title: "My Auto Merge Title",
+      automatic_merge: true
+      //automatic_merge_delay: 100,
+      //issue_options: {
+      //  labels: ["Label 1", "Label 2"],
+      //  assignees: ["carterm"]
+      //},
+      //review_options: {
+      //  reviewers: ["wpserviceuser"]
+      //}
+    }
+  });
+  const binaryString = "My Buffer Text 1";
+  const binaryData = Buffer.from(binaryString);
+  rootTree.addFile("Root File.txt", "Root File Data");
+  rootTree.addFile("Root Buffer.txt", binaryData);
+  rootTree.addFile("Root Buffer2.txt", binaryData);
+
+  await rootTree.treePush();
+
+  console.log(JSON.stringify(rootTree.lastRunStats, null, 2));
 })();
